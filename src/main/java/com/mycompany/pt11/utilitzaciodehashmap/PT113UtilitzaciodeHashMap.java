ad532978fe234e9bd4ac4ed5cb065d1110adb6c3
ycaza <ycaza@DESKTOP-T1UA6VD>
2025-03-28 09:55:45 +0100

    Versió inicial

---
+++ src/main/java/com/mycompany/pt11/utilitzaciodehashmap/PT113UtilitzaciodeHashMap.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pt11.utilitzaciodehashmap;
+
+/**
+ *
+ * @author Bea i Yamila
+ */
+public class PT113UtilitzaciodeHashMap {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
