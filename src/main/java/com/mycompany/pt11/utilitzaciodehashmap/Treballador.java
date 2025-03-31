c85c29f03dc1523a57059420e5b4e2e04231865e
ycaza <ycaza@DESKTOP-T1UA6VD>
2025-03-31 18:48:09 +0200

    Classe principal i Treballador comentada

--- src/main/java/com/mycompany/pt11/utilitzaciodehashmap/Treballador.java
+++ src/main/java/com/mycompany/pt11/utilitzaciodehashmap/Treballador.java
@@ -5,14 +5,23 @@
 package com.mycompany.pt11.utilitzaciodehashmap;
 
 /**
- *
+ * Classe Treballador
  * @author Bea i Yamila
  */
 public class Treballador {
-    private String dni, nom;
-    private int edat;
-    private double altura;
     
+    // Declaració d'atributs del treballador
+    private String dni, nom; // String per guardar el dni i el nom
+    private int edat; // int per guardar l'edat
+    private double altura; // double per guarda l'alçada
+    
+    /**
+     * Constructor parametritzat de Treballador
+     * @param dni
+     * @param nom
+     * @param edat
+     * @param altura 
+     */
     public Treballador(String dni, String nom, int edat, double altura) {
         this.dni = dni;
         this.nom = nom;
@@ -20,25 +29,45 @@ public class Treballador {
         this.altura = altura;
     }
 
+    /**
+     * Getter del DNI
+     * @return dni
+     */
     public String getDni() {
         return dni;
     }
     
+    /**
+     * Getter del Nom
+     * @return nom
+     */
     public String getNom() {
         return nom;
     }
 
+    /**
+     * Getter de l'edat
+     * @return edat
+     */
     public int getEdat() {
         return edat;
     }
 
+    /**
+     * Getter de l'alçada
+     * @return alçada
+     */
     public double getAltura() {
         return altura;
     }
 
+    /**
+     * Mètode sobreescrit toString
+     * @return informació del treballador
+     */
     @Override
     public String toString() {
-        return "Treballador{" + "nom=" + nom + ", edat=" + edat + ", alçada=" + altura + '}';
+        return "Nom: " + nom + ", Edat: " + edat + ", Alçada: " + altura;
     }
     
 }
